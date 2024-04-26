d9bb606da682cb3d87bc5cf9784beb9b1a3af7f8
ka2718 <a>
2024-04-27 00:05:04 +0900

    make function

--- generate_opening.py
+++ generate_opening.py
@@ -47,40 +47,7 @@ class MaxHeap:
         return top_n_elements
 
 
-if __name__ == "__main__":
-    # 親子関係の存在しないノードの組のうち、各ノードの勝率が一定以上50%から離れておらず、できるだけ局面数が多くて各ノードの局面数が近くなるような組を選ぶ
-    # メモリ使用量に注意。240225時点で、20エポック分で1GBほど消費。
-    target_epoch = 3800
-    epoch_num_for_joseki = 100
-
-    min_size = 6  # これより探訪数が少ない定石は除外する
-    max_depth = 15  # これより深い定跡は作らない（メモリ節約のため）
-    max_win_rate_diff = 0.12
-    joseki_num = 100
-    n_cand = 3
-    #n_div_try = 10
-
-    #target_dir = KIFU_DIR
-    target_dir = os.path.join("other_records", "240329_3700kifu")
-    save_dir = os.path.join("application_data", "joseki")
-    os.makedirs(save_dir, exist_ok=True)
-
-    start_epoch = target_epoch - epoch_num_for_joseki
-    end_epoch = target_epoch
-
-    all_kifu_list = []
-
-    for epoch in range(start_epoch, end_epoch):
-        files = os.listdir(os.path.join(target_dir, str(epoch)))
-        files = [file for file in files if file.endswith(".txt")]
-        for file in files:
-            with open(os.path.join(target_dir, str(epoch), file)) as fin:
-                kifu_text = fin.read()
-                kifu_list = kifu_text.splitlines()
-                kifu_list = [text.strip().split(",") for text in kifu_list]
-
-                all_kifu_list.extend(kifu_list)
-
+def generate_opening_tree(target_epoch, all_kifu_list):
     statevec2node = {}
     opening_tree = get_opening_node_from_state(State(), statevec2node)
     opening_tree.visited_num = 0
@@ -115,7 +82,7 @@ if __name__ == "__main__":
             if depth <= max_depth:
                 key = Glendenning2Official(normalized_action_str)
                 if key not in node.children.keys():
-                    node.children[key] = get_opening_node_from_state(state, statevec2node)
+                    node.children[key] = get_opening_node_from_state(normalized_state, statevec2node)
                     if isinstance(node.children[key], OpeningTree):
                         node.children[key].visited_num = 0
                         node.children[key].p1_win_num = 0
@@ -134,15 +101,54 @@ if __name__ == "__main__":
                 node.p1_win_num += 1
             else:
                 node.p2_win_num += 1
+                
+    return opening_tree
+
+
+if __name__ == "__main__":
+    # 親子関係の存在しないノードの組のうち、各ノードの勝率が一定以上50%から離れておらず、できるだけ局面数が多くて各ノードの局面数が近くなるような組を選ぶ
+    # メモリ使用量に注意。240225時点で、20エポック分で1GBほど消費。
+    target_epoch = 4067
+    epoch_num_for_joseki = 10
+
+    min_size = 6  # これより探訪数が少ない定石は除外する
+    max_depth = 15  # これより深い定跡は作らない（メモリ節約のため）
+    max_win_rate_diff = 0.15
+    joseki_num = 100
+    n_cand = 3
+    #n_div_try = 10
+
+    #target_dir = KIFU_DIR
+    target_dir = os.path.join("other_records", "240423")
+    save_dir = os.path.join("application_data", "joseki")
+    os.makedirs(save_dir, exist_ok=True)
+
+    start_epoch = target_epoch - epoch_num_for_joseki
+    end_epoch = target_epoch
+
+    all_kifu_list = []
+
+    for epoch in range(start_epoch, end_epoch):
+        files = os.listdir(os.path.join(target_dir, str(epoch)))
+        files = [file for file in files if file.endswith(".txt")]
+        for file in files:
+            with open(os.path.join(target_dir, str(epoch), file)) as fin:
+                kifu_text = fin.read()
+                kifu_list = kifu_text.splitlines()
+                kifu_list = [text.strip().split(",") for text in kifu_list]
+
+                all_kifu_list.extend(kifu_list)
 
     def traverse(tree, actions):
-        if tree.visited_num / tree.game_num >= 0.01:
+        if tree.visited_num / tree.game_num >= 0.005:
             print(actions)
             print("{} ({:.2f}%), p1 win rate = {:.2f}%".format(tree.visited_num, tree.visited_num / tree.game_num * 100, tree.p1_win_num / tree.visited_num * 100))
             print()
         for key, node in tree.children.items():
             if isinstance(node, OpeningTree):
                 traverse(node, actions + [key])
+
+    opening_tree = generate_opening_tree(target_epoch, all_kifu_list)
 
     traverse(opening_tree, [])
 
@@ -180,7 +186,6 @@ if __name__ == "__main__":
             break
         node_s, (node, action_list) = cands.pop()
         for action in node.children.keys():
-            #child = move_to_child(node, action, statevec2node)
             if isinstance(node.children[action], OpeningTree):
                 child = node.children[action]
             else:
