ba98548c3c09d3bee401ae380edb978b94e66c59
ka2718 <a>
2024-04-28 20:10:52 +0900

    analyze multiple times

--- analyze_opening.py
+++ analyze_opening.py
@@ -29,35 +29,41 @@ if __name__ == "__main__":
     target_openings = [x[1] for x in target_opening_data]
 
     # メモリ使用量に注意。240225時点で、20エポック分で1GBほど消費。
-    target_epoch = 4067
-    epoch_num_for_joseki = 10
+    target_epoch = 4000
+    epoch_num_for_analyze = 30
+    each_epoch_num_for_analyze = 10
 
     max_depth = 15  # これより深い定跡は作らない（メモリ節約のため）
 
-    target_dir = os.path.join("other_records", "240423")
+    target_dir = os.path.join("other_records", "240417_3900kifu")
     save_dir = os.path.join("application_data", "joseki")
     os.makedirs(save_dir, exist_ok=True)
 
-    start_epoch = target_epoch - epoch_num_for_joseki
-    end_epoch = target_epoch
+    start_epoch_all = target_epoch - epoch_num_for_analyze
+    end_epoch_all = target_epoch
 
-    all_kifu_list = []
+    for start_epoch in range(start_epoch_all, end_epoch_all, each_epoch_num_for_analyze):
+        end_epoch = start_epoch + each_epoch_num_for_analyze
+        print("="*50)
+        print(start_epoch, end_epoch)
 
-    for epoch in range(start_epoch, end_epoch):
-        files = os.listdir(os.path.join(target_dir, str(epoch)))
-        files = [file for file in files if file.endswith(".txt")]
-        for file in files:
-            with open(os.path.join(target_dir, str(epoch), file)) as fin:
-                kifu_text = fin.read()
-                kifu_list = kifu_text.splitlines()
-                kifu_list = [text.strip().split(",") for text in kifu_list]
+        all_kifu_list = []
 
-                all_kifu_list.extend(kifu_list)
+        for epoch in range(start_epoch, end_epoch):
+            files = os.listdir(os.path.join(target_dir, str(epoch)))
+            files = [file for file in files if file.endswith(".txt")]
+            for file in files:
+                with open(os.path.join(target_dir, str(epoch), file)) as fin:
+                    kifu_text = fin.read()
+                    kifu_list = kifu_text.splitlines()
+                    kifu_list = [text.strip().split(",") for text in kifu_list]
 
-    opening_tree, statevec2node = generate_opening_tree(target_epoch, all_kifu_list, max_depth)
+                    all_kifu_list.extend(kifu_list)
 
-    for target_opening in target_openings:
-        state, state_vec, _ = get_normalized_state(list(map(Official2Glendenning, target_opening)))
-        tree = statevec2node[state_vec]
-        print("{} ({:.2f}%), p1 win rate = {:.2f}%".format(tree.visited_num, tree.visited_num / tree.game_num * 100, tree.p1_win_num / tree.visited_num * 100))
+        opening_tree, statevec2node = generate_opening_tree(target_epoch, all_kifu_list, max_depth)
+
+        for target_opening in target_openings:
+            state, state_vec, _ = get_normalized_state(list(map(Official2Glendenning, target_opening)))
+            tree = statevec2node[state_vec]
+            print("{} ({:.2f}%), p1 win rate = {:.2f}%".format(tree.visited_num, tree.visited_num / tree.game_num * 100, tree.p1_win_num / tree.visited_num * 100))
 
