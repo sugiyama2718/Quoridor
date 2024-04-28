58d7584bedbf28d763add3558d6163e17ae1d432
ka2718 <a>
2024-04-28 13:26:33 +0900

    add names

--- analyze_opening.py
+++ analyze_opening.py
@@ -20,17 +20,19 @@ from Tree import OpeningTree
 
 
 if __name__ == "__main__":
-    target_openings = [  # official notation
-        ['e2', 'e8', 'e3', 'e7', 'e4', 'e6', 'd3h', 'c6h', 'd5v'],
-        ['e2', 'e8', 'e3', 'e7', 'e4', 'e6', 'd3h', 'c6h', 'e6v'],
-        ['e2', 'e8', 'e3', 'e7', 'e4', 'e6', 'a3h'],
-        ['e2', 'e8', 'e3', 'd3h'],
-        ['e2', 'c7h', 'e3', 'e8']
+    target_opening_data = [  # official notation
+        ("d3h, c6h, d5v", ['e2', 'e8', 'e3', 'e7', 'e4', 'e6', 'd3h', 'c6h', 'd5v']),
+        ("d3h, c6h, e6v", ['e2', 'e8', 'e3', 'e7', 'e4', 'e6', 'd3h', 'c6h', 'e6v']),
+        ("a3h", ['e2', 'e8', 'e3', 'e7', 'e4', 'e6', 'a3h']),
+        ("e2, e8, e3, d3h", ['e2', 'e8', 'e3', 'd3h']),
+        ("e2, e8, e3, c7h", ['e2', 'c7h', 'e3', 'e8'])
     ]
+    target_opening_names = [x[0] for x in target_opening_data]
+    target_openings = [x[1] for x in target_opening_data]
 
     # メモリ使用量に注意。240225時点で、20エポック分で1GBほど消費。
     target_epoch = 4067
-    epoch_num_for_joseki = 3
+    epoch_num_for_joseki = 10
 
     min_size = 6  # これより探訪数が少ない定石は除外する
     max_depth = 15  # これより深い定跡は作らない（メモリ節約のため）
