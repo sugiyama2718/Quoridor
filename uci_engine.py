b0471cc1d161b81803ac603e64e6e59b3d77d4d7
ka2718 <a>
2024-05-11 23:49:27 +0900

    implement go

--- uci_engine.py
+++ uci_engine.py
@@ -1,10 +1,13 @@
 import os, sys
+from State import State
+from Agent import actionid2str
+from util import Glendenning2Official
 
 # tensorflowのログを抑制
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # 標準エラー出力を抑制
-sys.stderr = open(os.devnull, 'w')
+#sys.stderr = open(os.devnull, 'w')
 
 from engine_util import prepare_AI
 import time
@@ -15,6 +18,7 @@ PARAMETER_PATH = os.path.join(DATA_BASE_DIR, "parameter")
 class UCIEngine:
     def __init__(self):
         self.AIs = None
+        self.state = None
 
     def handle_uci(self):
         print("id name ka Quoridor")
@@ -22,15 +26,21 @@ class UCIEngine:
         print("uciok")
 
     def handle_newgame(self):
-        print("New game started")
         self.AIs = [
             prepare_AI(PARAMETER_PATH, 0, search_nodes=1000, tau=0.32, level=-1, seed=int(time.time())),
             prepare_AI(PARAMETER_PATH, 1, search_nodes=1000, tau=0.32, level=-1, seed=int(time.time()))
         ]
-        # TODO: 色はどうやって判断するか？
+        self.state = State()
 
     def handle_go(self):
-        print("Go command received, calculating best move...")
+        if self.AIs is None:
+            self.handle_newgame()
+
+        color = self.state.turn & 2
+        action_id, _, _, v_post, _ = self.AIs[color].act_and_get_pi(self.state, use_prev_tree=True)
+        #print("score= {}, use_prev_tree={}".format(int(1000 * v_post), self.use_prev_tree))
+        action = Glendenning2Official(actionid2str(self.state, action_id))
+        print(f"bestmove {action}")
 
     def handle_quit(self):
         print("Quitting the program")
