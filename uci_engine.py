fb1f3906f67e6571fe21de0752037fe65e7100da
ka2718 <a>
2024-05-12 13:09:25 +0900

    handle prev_tree

--- uci_engine.py
+++ uci_engine.py
@@ -19,6 +19,9 @@ class UCIEngine:
     def __init__(self):
         self.AIs = None
         self.state = None
+        self.use_prev_trees = [None, None]
+
+        self.bestmoves = [None, None]
 
     def handle_uci(self):
         print("id name ka Quoridor")
@@ -31,22 +34,38 @@ class UCIEngine:
             prepare_AI(PARAMETER_PATH, 1, search_nodes=1000, tau=0.32, level=-1, seed=int(time.time()))
         ]
         self.state = State()
+        self.use_prev_trees = [True, True]
+        self.bestmoves = [None, None]
 
     def handle_go(self):
         if self.AIs is None:
             self.handle_newgame()
 
         color = self.state.turn % 2
-        action_id, _, _, v_post, _ = self.AIs[color].act_and_get_pi(self.state, use_prev_tree=True)
-        #print("score= {}, use_prev_tree={}".format(int(1000 * v_post), self.use_prev_tree))
+        action_id, _, _, v_post, _ = self.AIs[color].act_and_get_pi(self.state, use_prev_tree=self.use_prev_trees[color])
+        print("score= {}, use_prev_tree={}".format(int(1000 * v_post), self.use_prev_trees[color]))
         action = Glendenning2Official(actionid2str(self.state, action_id))
         print(f"bestmove {action}")
+        self.bestmoves[color] = action
 
     def handle_makemove(self, action):
         # official notation
+
+        if self.AIs is None:
+            self.handle_newgame()
+
+        color = self.state.turn % 2
+
         if not self.state.accept_action_str(Official2Glendenning(action)):
             print(action)
             print("this action is impossible")
+            return
+        
+        if self.bestmoves[color] is not None and self.bestmoves[color] == action:
+            self.use_prev_trees[color] = True
+            self.AIs[1 - color].prev_action = action
+        else:
+            self.use_prev_trees[color] = False
 
     def handle_quit(self):
         print("Quitting the program")
