d851c4ac99ae3a66994c936ee842f8d7fa229601
ka2718 <a>
2025-01-27 14:00:43 +0000

    fix

--- generate_opening_tree_by_selfplay.py
+++ generate_opening_tree_by_selfplay.py
@@ -14,7 +14,7 @@ from multiprocessing import Pool
 from tqdm import tqdm
 import argparse
 import logging
-from util import get_epoch_dir_name, generate_opening_tree, save_tree_graph, compute_contributions, update_opening_tree_with_new_kifu, MCTS_select, remove_nodes_below_threshold, select_and_get_nodess_and_actionss, get_normalized_action_list, mirror_action, update_array_with_beta
+from util import get_epoch_dir_name, generate_opening_tree, save_tree_graph, compute_contributions, update_opening_tree_with_new_kifu, MCTS_select, remove_nodes_below_threshold, select_and_get_nodess_and_actionss, get_normalized_action_list, mirror_action, update_array_with_beta, display_parameter, get_flipped_index
 tf.get_logger().setLevel(logging.ERROR)
 from State import State, accept_action_str, State_init, feature_int
 from Tree import Glendenning2Official
@@ -41,23 +41,58 @@ def selfplay_cycle(
     # print(opening_tree.P)
     if opening_tree.P is None:
         actionss = [[]] * game_num  # 初期局面の場合のみ、空リストで埋める
+        nodess = [None] * game_num
     else:
-        _, actionss = select_and_get_nodess_and_actionss(opening_tree, 10.0, 0, 0, game_num, game_num, 1)
+        nodess, actionss = select_and_get_nodess_and_actionss(opening_tree, 10.0, 0, 0, game_num, game_num, 1)
 
-    for actions in actionss:
+    new_actionss = []
+    for nodes, actions in zip(nodess, actionss):
         s = State()
+        mirror_s = State()
         State_init(s)
+        State_init(mirror_s)
+
         is_success = True
+        prev_is_mirrored = False
+
+        new_actions = []
+
         for action in actions:
+            if prev_is_mirrored:
+                action = get_flipped_index(action)
             action_str = actionid2str(s, action)
+            new_actions.append(action)
+            
+            mirror_action = get_flipped_index(action)
+            mirror_action_str = actionid2str(mirror_s, mirror_action)
+
             is_success = is_success and accept_action_str(s, action_str)
+            is_success = is_success and accept_action_str(mirror_s, mirror_action_str)
+
+            state_vec = tuple(feature_int(s).flatten())
+            mirror_state_vec = tuple(feature_int(mirror_s).flatten())
+
+            is_mirrored = (state_vec > mirror_state_vec)
+
             official_str = Glendenning2Official(action_str)
-            print(official_str, end=", ")
+            print(official_str, int(is_mirrored), end=", ")
+            #print(official_str, end=", ")
+
+            prev_is_mirrored = is_mirrored
+        new_actionss.append(new_actions)
 
         print()
         if not is_success:
             print("contains illegal move!!!")
-            
+            if nodes is not None:
+                for node, action in zip(nodes, actions):
+                    print("-"*30)
+                    print(action)
+                    print(display_parameter(np.array(node.P * 1000, dtype=int)))
+                    print(display_parameter(np.array(node.tree_c.contents.N_arr, dtype=int)))
+                    print(display_parameter(np.array(np.array(node.tree_c.contents.Q_arr) * 1000, dtype=int)))
+
+    actionss = new_actionss
 
     # 1) 自己対戦
     args_list = []
